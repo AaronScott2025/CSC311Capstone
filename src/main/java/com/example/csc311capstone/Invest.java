6838834673b42d55772d589f3e9c771a66bc3048
AaronScott2025 <147019715+AaronScott2025@users.noreply.github.com>
2024-10-23 15:41:42 -0400

    Investment methods added

--- src/main/java/com/example/csc311capstone/Invest.java
+++ src/main/java/com/example/csc311capstone/Invest.java
@@ -9,4 +9,66 @@ package com.example.csc311capstone;
  */
 
 public class Invest {
+    private int initial;
+    private int years;
+    private int yearlyInvestment;
+
+    public Invest(int initial, int years, int yearlyInvestment) {
+        this.initial = initial;
+        this.years = years;
+        this.yearlyInvestment = yearlyInvestment;
+    }
+
+    public int getInitial() {
+        return initial;
+    }
+
+    public void setInitial(int initial) {
+        this.initial = initial;
+    }
+
+    public int getYears() {
+        return years;
+    }
+
+    public void setYears(int years) {
+        this.years = years;
+    }
+
+    public int getYearlyInvestment() {
+        return yearlyInvestment;
+    }
+
+    public void setYearlyInvestment(int yearlyInvestment) {
+        this.yearlyInvestment = yearlyInvestment;
+    }
+
+    public double[] eightPercent() { //S&P500
+        double[] compound = new double[years];
+        double temp = initial;
+        for(int i = 1; i <= years; i++) {
+            temp = yearlyInvestment + (temp*1.08);
+            compound[i] = temp;
+        }
+        return compound;
+    }
+    public double[] fivePercent() { //High yield savings account
+        double[] compound = new double[years];
+        double temp = initial;
+        for(int i = 1; i <= years; i++) {
+            temp = yearlyInvestment + (temp*1.05);
+            compound[i] = temp;
+        }
+        return compound;
+    }
+    public double[] threePercent() { //Bonds
+        double[] compound = new double[years];
+        double temp = initial;
+        for(int i = 1; i <= years; i++) {
+            temp = yearlyInvestment + (temp*1.03);
+            compound[i] = temp;
+        }
+        return compound;
+    }
+
 }
