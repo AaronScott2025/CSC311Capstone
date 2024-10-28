c1a1c9f26255dba171b6b4c557ca19dfa06f3118
AaronScott2025 <147019715+AaronScott2025@users.noreply.github.com>
2024-10-27 20:44:44 -0400

    "Create Account" Scene, and Database Created

--- src/main/java/com/example/csc311capstone/User.java
+++ src/main/java/com/example/csc311capstone/User.java
@@ -5,7 +5,7 @@ package com.example.csc311capstone;
  * User:
  * The database will have the data for each user, as well as password, location, salary, and storage
  * of any previously generated data (IE: Function 4’s table data!). This will allow users to pick up right
- * where they left off.
+ * where they left off. SEE SQL
  */
 public class User {
 }
