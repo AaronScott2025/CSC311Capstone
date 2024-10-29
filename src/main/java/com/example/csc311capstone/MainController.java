4738756bd4e13d934782eef414b2b8d208cdf23a
AaronScott2025 <aaronscot2002@gmail.com>
2024-10-29 13:12:05 -0400

    Invest Controller began

--- src/main/java/com/example/csc311capstone/MainController.java
+++ src/main/java/com/example/csc311capstone/MainController.java
@@ -2,7 +2,11 @@ package com.example.csc311capstone;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Scene;
+import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 public class MainController {
 
@@ -37,10 +41,39 @@ public class MainController {
     void relocatePressed(ActionEvent event) {
 
     }
+    @FXML
+    void backToMain(ActionEvent event) { //Every function uses this.
+
+    }
 
     /**
+     * ========================================================================================================================================================== *
      * Function 1 Controller methods
+     * ========================================================================================================================================================== *
      */
+    @FXML
+    private Button exitBTN;
+
+    @FXML
+    private Button goBTN;
+
+    @FXML
+    private TextField startingTXT;
+
+    @FXML
+    private TextField yearlyTXT;
+
+    @FXML
+    private TextField yearsTXT;
+
+
+    @FXML
+    void showInvestments(ActionEvent event) { //Make new scene with graph, showing data.
+        Invest i = new Invest(Integer.getInteger(startingTXT.getText()),Integer.getInteger(yearsTXT.getText()),Integer.getInteger(yearlyTXT.getText()));
+        Stage stage = new Stage();
+//        Scene func1Scene = new Scene();
+//        stage.setScene();
+    }
 
 
 }
